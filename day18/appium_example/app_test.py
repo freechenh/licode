e6f782dba9774a9f433bc4d64305c11ace82f221
Administrator <876374178@qq.com>
2020-11-12 22:19:25 +0800

    提交appium的demo

--- day18/appium_example/app_test.py
+++ day18/appium_example/app_test.py
@@ -9,21 +9,21 @@ from time import sleep
 # = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 
 
-def file_path(p):
-    return os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+# def file_path(p):
+#     return os.path.abspath(os.path.join(os.path.dirname(__file__), p))
 
 
 @pytest.fixture()
 def web_set_up():
     desired_caps = dict()
     desired_caps['platformName'] = 'Android'
-    desired_caps['platformVersion'] = '5.0.1'
+    desired_caps['platformVersion'] = '5.1.1'
     desired_caps['deviceName'] = 'emulator-5554'
     desired_caps['appPackage'] = "io.appium.android.apis"
     desired_caps['appActivity'] = "io.appium.android.apis.ApiDemos"
-    desired_caps['unicodeKeyboard'] = "True"
-    desired_caps['resetKeyboard'] = "True"
-    driver = webdriver.Remote("localhost:4723/wd/hub", desired_caps)
+    desired_caps['unicodeKeyboard'] = True
+    desired_caps['resetKeyboard'] = True
+    driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
     yield driver
 
 
