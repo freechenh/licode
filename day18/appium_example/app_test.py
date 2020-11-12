05e2221ae3ed5e254439118f16392657a21336d4
Administrator <876374178@qq.com>
2020-11-12 21:33:12 +0800

    提交appium的demo

---
+++ day18/appium_example/app_test.py
@@ -0,0 +1,38 @@
+import os
+
+import pytest
+from appium import webdriver
+
+from time import sleep
+
+
+# = lambda p: os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+
+
+def file_path(p):
+    return os.path.abspath(os.path.join(os.path.dirname(__file__), p))
+
+
+@pytest.fixture()
+def web_set_up():
+    desired_caps = dict()
+    desired_caps['platformName'] = 'Android'
+    desired_caps['platformVersion'] = '5.0.1'
+    desired_caps['deviceName'] = 'emulator-5554'
+    desired_caps['appPackage'] = "io.appium.android.apis"
+    desired_caps['appActivity'] = "io.appium.android.apis.ApiDemos"
+    desired_caps['unicodeKeyboard'] = "True"
+    desired_caps['resetKeyboard'] = "True"
+    driver = webdriver.Remote("localhost:4723/wd/hub", desired_caps)
+    yield driver
+
+
+@pytest.fixture()
+def web_tear_down():
+    yield
+
+
+class TestApp:
+
+    def test_open_app(self, web_set_up):
+        pass
