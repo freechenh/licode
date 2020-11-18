ba7c6258de167b5430ea6b2294d1a0c319ba1006
Administrator <876374178@qq.com>
2020-11-18 22:43:07 +0800

    提交appium的demo

--- day19/jingdong.py
+++ day19/jingdong.py
@@ -19,6 +19,16 @@ find_ = "//input[@id='key']"
 
 search_button = "//button[@class='button']"
 
+myself_button = '//li[@id="ttbar-myjd"]//a[contains(text(), "我的京东")]'
+
+my_focus = '//dd[@id="_MYJD_product"]/a[contains(text(), "关注商品")]'
+
+maotai = '//div[@id="goods_100012043978"]/div[@class="item-inner"]//a[contains(text(), "茅台")]'
+
+num_add = "//a[@class='btn-add']"
+
+add_cart = "//a[@id='InitCartUrl']"
+
 
 class Jd:
     def __init__(self):
@@ -30,22 +40,64 @@ class Jd:
 
         self.driver.get(url)
 
-    def query_maotai(self):
+    def find_element_exist(self, ele_xpath):
         while True:
             try:
-                find_text = self.driver.find_element(By.XPATH, find_)
-                find_text.send_keys("茅台")
-                click_button = self.driver.find_element(By.XPATH, search_button)
-                click_button.click()
-                break
+                ele = self.driver.find_element(By.XPATH, ele_xpath)
+                return ele
             except NoSuchElementException:
                 pass
+
+    def query_maotai(self):
+        find_text = self.find_element_exist(find_)
+        find_text.send_keys("茅台")
+        click_button = self.find_element_exist(search_button)
+        click_button.click()
+
+    def enter_myself(self):
+        enter_ = self.find_element_exist(myself_button)
+        enter_.click()
+        self.switch_frame()
+
+    def enter_my_focus(self):
+        enter_ = self.find_element_exist(my_focus)
+        enter_.click()
+
+    def enter_goods_page(self):
+        enter_ = self.find_element_exist(maotai)
+        enter_.click()
+        self.switch_frame()
+
+    def switch_frame(self, n=-1):
+        window = self.driver.window_handles
+        self.driver.switch_to.window(window[n])
+
+    def num_add_one(self):
+        num = self.find_element_exist(num_add)
+        num.click()
+
+    def cart_add(self):
+        cart = self.find_element_exist(add_cart)
+        cart.click()
 
     def quit(self):
         self.driver.quit()
 
 
-if __name__ == '__main__':
+def main():
     demo = Jd()
-    demo.query_maotai()
+    demo.enter_myself()
+    time.sleep(2)
+    # demo.switch_frame()
+    time.sleep(2)
+    demo.enter_my_focus()
+    time.sleep(2)
+    demo.enter_goods_page()
+    time.sleep(10)
+    # demo.switch_frame()
+    time.sleep(10)
     demo.quit()
+
+
+if __name__ == '__main__':
+    main()
