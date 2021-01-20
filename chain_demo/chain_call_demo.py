782574033a5d0dd86a49b837424a4869234677d4
chenhui <chui@ruijie.com.cn>
2021-01-20 17:50:46 +0800

    ````````

--- chain_demo/chain_call_demo.py
+++ chain_demo/chain_call_demo.py
@@ -6,3 +6,29 @@
 # @Desc: 
 # @Project: licode
 # @Source: PyCharm
+
+
+class Person:
+
+    # def __init__(self):
+    #     self.name = None
+    #     self.age = None
+
+    def name(self, name):
+        self.name = name
+        return self
+
+    def age(self, age):
+        self.age = age
+        return self
+
+    def show(self):
+        print(self.name, self.age)
+
+
+if __name__ == '__main__':
+    p = Person()
+    (p
+     .name('tyh')
+     .age(15)
+     .show())
