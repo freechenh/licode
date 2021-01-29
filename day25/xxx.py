774fd4678c9534d0a5e65bcf43855bbb835241b9
chenhui <chui@ruijie.com.cn>
2021-01-29 16:46:59 +0800

    ````````

---
+++ day25/xxx.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @File: xxx.py
+# @Author: dell
+# @Date: 2021/1/25  16:35
+# @Desc: 
+# @Project: licode
+# @Source: PyCharm
+
+
+stations = dict()
+stations['kone'] = set(['id', 'nv', 'ut'])
